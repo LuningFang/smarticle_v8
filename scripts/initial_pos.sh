7cbb7618f541d591cc12efd1ec6efb81b733d9c2
LuningFang <lfang9@wisc.edu>
2023-01-14 13:31:41 -0600

    modify script for Euler

--- scripts/initial_pos.sh
+++ scripts/initial_pos.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-filename='commands_21Rotations_Exchange.txt'
-solver=1
+jobid=3
+
+filenames=("commands_21Rotations.txt" "commands_21Rotations.txt" "commands_21Rotations_Exchange.txt" "commands_21Rotations_Exchange.txt")
+solvers=(0 1 0 1)
+filename=${filenames[$jobid]}
+solver=${solvers[jobid]}
+
+echo "txt $filename solver $solver"
 while read line; do
 # reading each line and pass arguments to chrono
 		echo "Line: $line"
